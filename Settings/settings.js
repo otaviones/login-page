977c71995c72cf5ffa8986dbee224c468ed3cfc2
otaviones <azeredo50@gmail.com>
2024-11-13 16:14:59 -0300

    v2

--- Settings/settings.js
+++
@@ -1,7 +0,0 @@
-echo "# login-page" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/otaviones/login-page.git
-git push -u origin main
